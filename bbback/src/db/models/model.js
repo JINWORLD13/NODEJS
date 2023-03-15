4faae5471ac8a8960a78f92d316c081d3764ea38
KimYohan <thgks486@gmail.com>
2023-03-15 19:40:05 +0900

    로그인,회원가입 완료

--- bbback/src/db/models/model.js
+++ bbback/src/db/models/model.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const UserSchema = require("../schemas/user");
 const GraphSchema = require("../schemas/graph");
+const ConstituencySchema = require("../schemas/constituency");
 
+exports.Constituency = mongoose.model("Constituency", ConstituencySchema);
 exports.User = mongoose.model("User", UserSchema);
 exports.Graph = mongoose.model("Graph", GraphSchema);
--- bbback/src/routers/districts.js
+++ bbback/src/routers/districts.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const buildResponse = require("../utils/buildResponse")
-
+const buildResponse = require("../utils/buildResponse");
+const { Districts } = require("../db/models/model");
 
 router.get("/districts", async (req, res, next) => {
   try {
-    let districtsData = "db에서 지역명 뽑는 방식"
+    let districtsData = await Districts.find();
     res.status(200).json(buildResponse(districtsData));
   } catch (err) {
     next(err);
