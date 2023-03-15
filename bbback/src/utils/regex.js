81cf1361a4cec64d598f994a4fa0a88aa8bc2fcb
KimYohan <thgks486@gmail.com>
2023-03-15 23:41:03 +0900

    회원가입 유효성 검사 완료

--- bbback/src/utils/regex.js
+++ bbback/src/utils/regex.js
@@ -1,37 +1,16 @@
-// const Regex = (props) => {
-//   const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/i;
-//   const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
-//   const nameRegex = /^[가-힣]{2,}$/u;
-//   const phoneNumberRegex = /^\d{11}$/;
-
-//   if (nameRegex.test(props) === true) {
-//     return true;
-//   } else if (emailRegex.test(props) === true) {
-//     return true;
-//   } else if (passwordRegex.test(props) === true) {
-//     return true;
-//   } else if (phoneNumberRegex.test(props) === true) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
 const Regex = (input, type) => {
   switch (type) {
-    case 'email':
+    case "email":
       return /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/i.test(input);
-    case 'password':
+    case "password":
       return /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/.test(input);
-    case 'name':
+    case "name":
       return /^[a-zA-Z가-힣]{2,}$/.test(input);
-    case 'phone':
+    case "phone":
       return /^\d{2,3}-\d{3,4}-\d{4}$|^\d{10,11}$/.test(input);
     default:
       return false;
   }
 };
 
-export default Regex;
-
-// module.exports = Regex;
+module.exports = Regex;
